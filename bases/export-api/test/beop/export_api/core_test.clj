62591cfa27fb999333a7d3387d2b39c184981f55
Romain Patrelle <rpatrelle@beop.io>
2024-02-23 20:41:41 +0100

    fix local/root key issue

--- bases/export-api/test/beop/export_api/core_test.clj
+++ bases/export-api/test/beop/export_api/core_test.clj
@@ -1,10 +1,42 @@
 (ns beop.export-api.core-test
-  (:require [clojure.test :as test :refer [deftest is]]
-            [beop.resources.test-system :as test-system]
-            [beop.export-api.core :as core]))
+  (:require [beop.resources.test-system :as test-system]
+            [camel-snake-kebab.core :refer [->snake_case]]
+            [clojure.test :as test :refer [deftest is use-fixtures]]
+            [honey.sql :as sql]
+            [next.jdbc :as jdbc]
+            [next.jdbc.result-set :as rs]))
 
-(defn prepare-mysql-fixture []
-  (let []))
+(def a-participation {:id "1"
+                      :content_account "aaaaaaaaaaaaaaaaaaaaaaaa"
+                      :widget_account "aaaaaaaaaaaaaaaaaaaaaaaa"
+                      :wrapper_id "cccccccccccccccccccccccc"
+                      :viewer_account "aaaaaaaaaaaaaaaaaaaaaaa1"
+                      :form_entry "a-form"
+                      :optins_entries "an-optin"})
+
+(defn insert-data-in-participations []
+  (let [mysql-participations-conn (-> test-system/deps :mysql :participations-conn :writer :datasource)]
+    (jdbc/execute! mysql-participations-conn (sql/format {:insert-into :participations
+                                                          :values [a-participation]}))))
+
+(defn empty-participations []
+  (let [mysql-participations-conn (-> test-system/deps :mysql :participations-conn :writer :datasource)]
+    (jdbc/execute! mysql-participations-conn ["TRUNCATE TABLE participations"])))
+
+(defn participation-data-fixture [f]
+  (insert-data-in-participations)
+  (f)
+  (empty-participations))
+
+(use-fixtures :once
+  test-system/with-test-system
+  participation-data-fixture)
 
 (deftest dummy-test
-  (is (= 1 1)))
+  (let [mysql-participations-conn (-> test-system/deps :mysql :participations-conn :writer :datasource)
+        participation-in-mysql (jdbc/execute! mysql-participations-conn
+                                              (sql/format {:select :*
+                                                           :from :participations})
+                                              {:builder-fn rs/as-unqualified-modified-maps
+                                               :label-fn ->snake_case})]
+    (is (= [a-participation] participation-in-mysql))))
